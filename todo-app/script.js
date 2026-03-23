1e10efa3e48261e38a5c5eea24fffa7c80abbe5a
Rishav Sharma <rishavme5@gmail.com>
2026-03-23 14:33:43 +0545

    Day 2 To-Do App

---
+++ todo-app/script.js
@@ -0,0 +1,45 @@
+function addTask() {
+  const input = document.getElementById("taskInput");
+  const taskText = input.value;
+
+  if (taskText === "") return;
+
+  const li = document.createElement("li");
+
+  li.innerHTML = `
+    <span onclick="toggleTask(this)">${taskText}</span>
+    <button onclick="deleteTask(this)">X</button>
+  `;
+
+  document.getElementById("taskList").appendChild(li);
+
+  input.value = "";
+
+  saveTasks();
+}
+
+function deleteTask(button) {
+  button.parentElement.remove();
+  saveTasks();
+}
+
+function toggleTask(task) {
+  task.classList.toggle("completed");
+  saveTasks();
+}
+
+/* SAVE TO LOCAL STORAGE */
+function saveTasks() {
+  const tasks = document.getElementById("taskList").innerHTML;
+  localStorage.setItem("tasks", tasks);
+}
+
+/* LOAD TASKS WHEN PAGE OPENS */
+function loadTasks() {
+  const saved = localStorage.getItem("tasks");
+  if (saved) {
+    document.getElementById("taskList").innerHTML = saved;
+  }
+}
+
+loadTasks();
